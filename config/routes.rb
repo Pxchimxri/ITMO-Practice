579d8bdb82d5e6f38c7aa723646b633b80dd1e15
Pachimari <65122232+Pxchimxri@users.noreply.github.com>
2022-06-20 11:17:32 +0300

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
 
-  root "users#index"
-  resources :orders #plural
-  resources :users #plural
-  resources :options #plural
+  root 'users#index'
+  resources :orders # plural
+  resources :users # plural
+  resources :options # plural
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_171413) do
     t.boolean "active", null: false
     t.string "role", null: false
     t.integer "cur_order_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "messages", "orders"
