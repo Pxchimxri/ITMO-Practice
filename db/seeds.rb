414986afdb02e9105ec7bc7711b6b71fdc5ac2ec
Nikita <baby0benz@gmail.com>
2022-06-21 01:55:39 +0300

    seed db with more records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.create(name: "Admin", role: "admin", active: true)
+clients = User.create([{name: "Client1", role: "client", active: true}, {name: "Client2", role: "client", active: true}])
+drivers = User.create([{name: "Driver1", role: "driver", active: true}, {name: "Driver2", role: "driver", active: true}])
+
+Order.create(price: 10.1, interest_rate: 0.1, tariff: "standard", status: "finished", from: "Pushkina", to: "Kolotushkina", client: clients.first, driver: drivers.first)
+Order.create(price: 12345.54321, interest_rate: 0.15, tariff: "standard", status: "started", from: "Sablina", to: "Grablina", client: clients.last)
+Order.create(price: 65.6, interest_rate: 0.5, tariff: "premium", status: "started", from: "Safina", to: "Grafina", client: clients.first, driver: drivers.last)
+Order.create(price: 15689.67, interest_rate: 0.1, tariff: "comfort", status: "finished", from: "Nikonorova", to: "Egorova", client: clients.first, driver: drivers.first)
