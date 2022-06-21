f74fa86c14de9f592050e84a4d4588f3a4072f9c
Pachimari <65122232+Pxchimxri@users.noreply.github.com>
2022-06-21 11:54:31 +0300

    Auth migration

--- db/migrate/20220607100434_create_users.rb
+++ db/migrate/20220607100434_create_users.rb
@@ -8,28 +8,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :role, null: false
       t.integer :cur_order_id
 
-      ## Database authenticatable
-      t.string :email,              null: false, default: ''
-      t.string :encrypted_password, null: false, default: ''
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
-
       t.timestamps
     end
-
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_171413) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_084052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_171413) do
     t.boolean "active", null: false
     t.string "role", null: false
     t.integer "cur_order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -69,8 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_171413) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
