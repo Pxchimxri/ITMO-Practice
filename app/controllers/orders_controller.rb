8b017904a8c64168e68329a8800e703524425ddf
Nikita <baby0benz@gmail.com>
2022-06-25 02:27:04 +0300

    add using of query objects

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,19 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
-    role = "adm1in" # TODO: replace hardcoded role with cancancan
-    if role == "client"
+    role = "client"                                                         #
+    if role == "client"                                                     # TODO: replace hardcoded role with cancancan
+      @orders = ClientOrdersHistoryQuery.call(client: User.client.first)    #
       ClientOrderListService.call(@orders)
     elsif role == "driver"
+      @orders = LookingForCarOrdersQuery.call
       DriverOrderListService.call(@orders)
     elsif role == "admin"
+      @orders = Order.all                                               # TODO: implement pagination
       @income_total = AdminOrderListService.call(@orders).result
     else
       invalid_role(role)
     end
-    render "order/index_#{role}"
+    render "orders/index_#{role}"
   end
 
   private
@@ -21,7 +23,7 @@ class OrdersController < ApplicationController
   end
 
   def invalid_role(role, msg = "Order index request was made with invalid role parameter #{role}", exception = ActionController::BadRequest)
-    Rails.logger.error(msg)
+    Rails.logger.warn(msg)
     raise exception.new, msg
   end
 end
