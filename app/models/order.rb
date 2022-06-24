bf3dfa4962fe1dcf2efe555b074f69f5600ac223
Nikita <baby0benz@gmail.com>
2022-06-25 02:20:48 +0300

    explicitly mark driver as optional

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   enum :tariff => {standard: "standard", comfort: "comfort", premium: "premium"}
   enum :status => {started: "started", on_way: "on_way", finished: "finished"}
   belongs_to :client, :class_name => "User"
-  belongs_to :driver, :class_name => "User"
+  belongs_to :driver, :class_name => "User", optional: true
   has_one :message
   has_many :order_options
   has_many :options, through: :order_options
