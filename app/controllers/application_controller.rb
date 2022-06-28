599c563fb4e0facced93b7493d7e7957aaa85562
Pachimari <65122232+Pxchimxri@users.noreply.github.com>
2022-06-28 17:37:57 +0300

    fix: add new hidden field for user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role active])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role active balance])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name role])
   end
 end
